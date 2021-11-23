04c70832d684804859a7f29b0267551ad5e2d3b4
Henrik Øberg Myhre <henrik.myhre99@gmail.com>
2021-11-23 22:13:54 +0100

    changed fasta to csv

--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,8 @@
 from Bio import SeqIO
 
 input_file = "NoGapsMSA_SIS1-on-top.fasta"
-seq_true_file = "seq_true.fasta"
-seq_false_file = "seq_false.fasta"
+seq_true_file = "seq_true.csv"
+seq_false_file = "seq_false.csv"
   
   
 def split_sequence(sequence):
@@ -14,14 +14,14 @@ def split_file(input_file, true_file, false_file):
   
   fasta_sequences = SeqIO.parse(open(input_file),'fasta')
   
-  seq_end_list = []
   seq_start_list = []
+  seq_end_list = []
   
   with open(true_file, "w") as seq_true_file:
     for fasta in fasta_sequences:
       name, sequence = fasta.id, str(fasta.seq)
       sequence_start, sequence_end = split_sequence(sequence)
-      seq_true_file.write(">" + name + "\n" + sequence_start + "\n" + sequence_end + "\n" + "1" + "\n")
+      seq_true_file.write(name + ";" + sequence_start + ";" + sequence_end + ";1\n")
       
       seq_start_list.append([name, sequence_start])
       seq_end_list.append([name, sequence_end])
@@ -31,8 +31,8 @@ def split_file(input_file, true_file, false_file):
   with open(false_file, "w") as seq_false_file:    
     for start in seq_start_list:
       for end in seq_end_list:
-        if start[0] != end[0]:
-          seq_false_file.write(">" + start[0] + " | " + end[0] + "\n" + start[1] + "\n" + end[1] + "\n" +"0" +"\n")
+        if start[0] == end[0]: continue
+        seq_false_file.write(start[0] + "<>" + end[0] + ";" + start[1] + ";" + end[1] + ";0\n")
         
     seq_false_file.close()
     
