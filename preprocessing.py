b2a433a8cc818a1dfa1b04d16b1e7f1df5b1f2b1
Henrik Øberg Myhre <henrik.myhre99@gmail.com>
2021-11-23 16:43:39 +0100

    preprocessing data sequences

--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,8 @@
 from Bio import SeqIO
 
 input_file = "NoGapsMSA_SIS1-on-top.fasta"
-seq_start_file = "seq_start.fasta"
-seq_end_file = "seq_end.fasta"
-shuffled_seg_end_file = "shuffled_seq_end.fasta"
+seq_true_file = "seq_true.fasta"
+seq_false_file = "seq_false.fasta"
   
   
 def split_sequence(sequence):
@@ -11,33 +10,31 @@ def split_sequence(sequence):
   start, end = sequence[:int(seq_len/2)], sequence[int(seq_len/2):]
   return start, end
   
-def split_file(input_file, start_file, end_file, shuffled_end_file):
+def split_file(input_file, true_file, false_file):
   
   fasta_sequences = SeqIO.parse(open(input_file),'fasta')
   
   seq_end_list = []
+  seq_start_list = []
   
-  with open(start_file, "w") as seq_start_file:
+  with open(true_file, "w") as seq_true_file:
     for fasta in fasta_sequences:
       name, sequence = fasta.id, str(fasta.seq)
       sequence_start, sequence_end = split_sequence(sequence)
-      seq_start_file.write(">" + name + "\n" + sequence_start + "\n")
+      seq_true_file.write(">" + name + "\n" + sequence_start + "\n" + sequence_end + "\n" + "1" + "\n")
       
+      seq_start_list.append([name, sequence_start])
       seq_end_list.append([name, sequence_end])
     
-    seq_start_file.close()
+    seq_true_file.close()
     
-  with open(end_file, "w") as seq_end_file, open(shuffled_end_file, "w") as shuffled_seq_end_file:
-    len_seq_end_list = len(seq_end_list)
-    
-    for i in range(len_seq_end_list):
-      seq_end_file.write(">" + seq_end_list[i][0] + "\n" + seq_end_list[i][1] + "\n")
-      
-    for i in range(1, len_seq_end_list):
-      for j in range(len_seq_end_list):
-        shuffled_seq_end_file.write(">" + seq_end_list[(i+j)%len_seq_end_list][0] + "\n" + seq_end_list[(i+j)%len_seq_end_list][1] + "\n")
+  with open(false_file, "w") as seq_false_file:    
+    for start in seq_start_list:
+      for end in seq_end_list:
+        if start[0] != end[0]:
+          seq_false_file.write(">" + start[0] + " | " + end[0] + "\n" + start[1] + "\n" + end[1] + "\n" +"0" +"\n")
         
-    seq_end_file.close()
-    shuffled_seq_end_file.close()
+    seq_false_file.close()
     
-split_file(input_file, seq_start_file, seq_end_file, shuffled_seg_end_file)
+split_file(input_file, seq_true_file, seq_false_file)
+
